62df218afd0ec732841137239a56453900bc8f8f
Tagaki Aki <a-tagaki@esm.co.jp>
2013-06-20 15:12:18 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anpikun::Application.config.secret_token = 'a41fddd39f050e685d2c19fa9acd0efdbb30055d66c6133beaa609a6653a10922a8bfef1ccc60bb9b012fc9cfda60b4c415bde200d19895a230acf2a01a1c213'
